74fb5eff0ca105f6ec6e98717b56786bc83f2b1b
aridontknowcode <2021457646@student.uitm.edu.my>
2024-02-29 17:33:02 +0800

    fixed eslint problem

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,26 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/email-template';
-import { Resend } from 'resend';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { EmailTemplate } from '../../components/email-template';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Harith <m.harith.hadi@gmail.com>',
-      to: ['m.harith.hadi@gmail.com'],
-      subject: 'Hello world',
-      text: 'Welcome, John!', // Provide plain text version of the email
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'Harith <m.harith.hadi@gmail.com>',
+//       to: ['m.harith.hadi@gmail.com'],
+//       subject: 'Hello world',
+//       text: 'Welcome, John!', // Provide plain text version of the email
+//       react: EmailTemplate({ firstName: 'John' }),
+//     });
 
-    if (error) {
-      res.status(400).json({ error });
-    }
+//     if (error) {
+//       res.status(400).json({ error });
+//     }
 
-    res.status(200).json({ data });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-}
+//     res.status(200).json({ data });
+//   } catch (error) {
+//     res.status(400).json({ error });
+//   }
+// }
