02ee50533e5580023348f2df44752051998110e7
aridontknowcode <2021457646@student.uitm.edu.my>
2024-03-02 10:55:25 +0800

    changed the aboutsection and herosection

--- components/AbouSection.jsx
+++ components/AbouSection.jsx
@@ -65,7 +65,7 @@ const AboutSection = () => {
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                 <p className="text-base lg:text-lg ">
-                I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
+                I am a new developer, currently going into next.js.
                 </p>
                 <div className="flex flex-row mt-8">
                     <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
